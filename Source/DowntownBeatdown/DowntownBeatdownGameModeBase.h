40cd87283456ba4940649cce12f1368413cc66f4
Marcus Rupley <mrupley@gmail.com>
2020-05-13 12:27:03 +0900

    DTB: initial commit with animated character

---
+++ Source/DowntownBeatdown/DowntownBeatdownGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DowntownBeatdownGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOWNTOWNBEATDOWN_API ADowntownBeatdownGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
